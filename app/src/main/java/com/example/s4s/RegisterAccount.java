da43a2c6a6fde15f7841e6642d1995b218e16dea
RaeVin Johnson <raevinjhnsn504@gmail.com>
2021-10-28 14:38:28 -0500

    RegisterAccount java and xml file updated

--- app/src/main/java/com/example/s4s/RegisterAccount.java
+++ app/src/main/java/com/example/s4s/RegisterAccount.java
@@ -3,12 +3,30 @@ package com.example.s4s;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterAccount extends AppCompatActivity {
+
+    EditText userName;
+    EditText userEmail;
+    EditText userPassword;
+    EditText goalsEntry;
+    Button registerBTN;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+
+        userName = findViewById(R.id.userName);
+        userEmail = findViewById(R.id.userEmail);
+        userPassword = findViewById(R.id.userPassword);
+        goalsEntry = findViewById(R.id.goalsEntry);
+        registerBTN = findViewById(R.id.registerBTN);
+        imageView = findViewById(R.id.imageView);
     }
 }
