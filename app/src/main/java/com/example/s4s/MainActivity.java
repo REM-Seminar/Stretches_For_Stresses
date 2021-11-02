fa2f4edd62143ee85bc42343481e26bf3150513f
JollyTurtles <eve.gaddis@yahoo.com>
2021-11-02 13:04:38 -0500

    Bottom Navigation Tab

--- app/src/main/java/com/example/s4s/MainActivity.java
+++ app/src/main/java/com/example/s4s/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.s4s;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.homeNavigation:
+                        break;
+                    case R.id.profileNavigation:
+                        break;
+                    case R.id.catalogNavigation:
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
+
 }
