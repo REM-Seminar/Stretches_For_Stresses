70fa5a465ad611c3ef8694b4b0977ca69a2f0547
JollyTurtles <eve.gaddis@yahoo.com>
2021-11-04 12:58:42 -0500

    All Fragments linked to Navigation Tab

--- app/src/main/java/com/example/s4s/fragments/CatalogFragment.java
+++ app/src/main/java/com/example/s4s/fragments/CatalogFragment.java
@@ -2,59 +2,40 @@ package com.example.s4s.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.s4s.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CatalogFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class CatalogFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String TAG = "CatalogFragment";
+    TextView tvSelect;
+    ImageButton ibUpperBody;
+    ImageButton ibLowerBody;
+    ImageButton ibFullBody;
+    ImageButton ibBack;
 
     public CatalogFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CatalogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CatalogFragment newInstance(String param1, String param2) {
-        CatalogFragment fragment = new CatalogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -62,5 +43,15 @@ public class CatalogFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_catalog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvSelect = view.findViewById(R.id.tvSelect);
+        ibUpperBody = view.findViewById(R.id.ibUpperBody);
+        ibLowerBody = view.findViewById(R.id.ibLowerBody);
+        ibFullBody = view.findViewById(R.id.ibFullBody);
+        ibBack = view.findViewById(R.id.ibBack);
     }
 }
