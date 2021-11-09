4b4009e41f2f85424d327011732867bde5ae14f9
Masonsh98 <mhughe12@bulldogs.aamu.edu>
2021-11-09 13:39:33 -0600

    Added Activity_catalog_screen fragment and Catalog Screen Class

---
+++ app/src/main/java/com/example/s4s/data/model/CatalogScreen.java
@@ -0,0 +1,15 @@
+package com.example.s4s.data.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class CatalogScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog_screen);
+    }
+}
